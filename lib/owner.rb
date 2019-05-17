05123f32a85f8f676877b9a71b56016237263e21
github username <github email address>
2019-05-16 20:51:30 -0400

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,77 @@
+require 'pry'
+
 class Owner
-  # code goes here
+  attr_accessor :name, :pets
+  attr_reader :species
+
+  @@all = []
+
+  def initialize(species)
+    @pets = {fishes: [], dogs: [], cats: []}
+    @species = species
+    @@all << self
+  end
+
+  def say_species
+    "I am a #{@species}."
+  end
+
+  def buy_fish(name)
+    @pets[:fishes] << Fish.new(name)
+  end
+
+  def buy_cat(name)
+    @pets[:cats] << Cat.new(name)
+  end
+
+  def buy_dog(name)
+    @pets[:dogs] << Dog.new(name)
+  end
+
+  def walk_dogs
+    @pets[:dogs].each do |dog|
+      dog.mood = "happy"
+    end
+  end
+
+  def play_with_cats
+    @pets[:cats].each do |cat|
+      cat.mood = "happy"
+    end
+  end
+
+  def feed_fish
+    @pets[:fishes].each do |fish|
+      fish.mood = "happy"
+    end
+  end
+
+  def sell_pets
+    @pets.each do |types, pets|
+      pets.each do |pet|
+        pet.mood = "nervous"
+      end
+    end
+    @pets.clear
+  end
+
+  def list_pets
+    fish_count = @pets[:fishes].count
+    cat_count = @pets[:cats].count
+    dog_count = @pets[:dogs].count
+    "I have #{fish_count} fish, #{dog_count} dog(s), and #{cat_count} cat(s)."
+  end
+
+
+  def self.all
+    @@all
+  end
+
+  def self.count
+    @@all.size
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
 end
